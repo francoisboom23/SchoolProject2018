1b22b4049499a3e5f12b0c7a21bcece12fba362b
francois <francois@francois-desktop>
2018-04-03 21:21:37 +0200

    final

--- SchoolProject2018/src/Main.java
+++ SchoolProject2018/src/Main.java
@@ -1,12 +1,6 @@
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello");
-		System.out.println("Hello2");
-		System.out.println("Hello3");
-		System.out.println("Hello4");
-		System.out.println("Hello5");
-		System.out.println("Hello6");
-
+		System.out.println("Vincent & François");
 	}
 }
