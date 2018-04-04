3909cf9da5daa9e7ebfd81c15ef00bc2a369ad0f
francois <francois@francois-laptop>
2018-04-04 20:48:54 +0200

    gt

--- SchoolProject2018/src/Main.java
+++ SchoolProject2018/src/Main.java
@@ -1,8 +1,9 @@
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Vincent & François");
+		System.out.println("Vincent & Francois");
 		System.out.println("2");
 		System.out.println("3");
 		System.out.println("4");
+		System.out.println("laptop");
 	}
 }
