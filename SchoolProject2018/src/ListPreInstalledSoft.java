7a6837c33794b3f46555b3b98fea12622576389c
francois <francois@francois-desktop>
2018-04-09 15:36:08 +0200

    gt

--- SchoolProject2018/src/ListPreInstalledSoft.java
+++ SchoolProject2018/src/ListPreInstalledSoft.java
@@ -10,18 +10,39 @@ import accessBD.TableModelGen;
 
 public class ListPreInstalledSoft extends JPanel{
 	
+	private JLabel PreInstalledSoftLabel;
+	private JComboBox combox;
+	private String[] table= {"lol","mdr","test"};
 	
 	public ListPreInstalledSoft(Connection connect) {
 		setBounds(0,0,500,500);
 		
 		try {
-			PreparedStatement prepStat = connect.prepareStatement("select * from Installation;");
-			TableModelGen table = AccessBDGen.creerTableModel(prepStat);
-		 	JTable table2 = new JTable(table);
-		 	table2.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
-		 	JScrollPane scroll = new JScrollPane (table2) ;
-		 	this.add(scroll);
+			PreparedStatement prepStat = connect.prepareStatement("SELECT Description FROM dbinstallations.TypePC;");
+			TableModelGen table2 = AccessBDGen.creerTableModel(prepStat);
+			
+			
+//			while(String.valueOf(table2.getValueAt(1, 0))!=null) {
+//				table[i]=String.valueOf(table2.getValueAt(i, 0));
+//				combox.addItem(String.valueOf(table2.getValueAt(1, 0)));
+//				System.out.println(String.valueOf(table2.getValueAt(i, 0)));
+//				i++;
+//			}
+			for(int i=0; i <= table2.getRowCount()-1; i++) {
+				System.out.println(String.valueOf(table2.getValueAt(i, 0)));
+				combox.addItem(String.valueOf(table2.getValueAt(i, 0)));
+
+			}
+//			table[1]=String.valueOf(table2.getValueAt(0, 0));
+//			PreInstalledSoftLabel = new JLabel(String.valueOf(table2.getValueAt(1, 0)));
+//			PreInstalledSoftLabel = new JLabel(String.valueOf(table2.getRowCount())); -> 5
 			}
 		catch(SQLException e) {	}
+		
+		PreInstalledSoftLabel = new JLabel("Type de PC:");
+		PreInstalledSoftLabel.setHorizontalAlignment(SwingConstants.RIGHT);
+		combox = new JComboBox(table);
+		this.add(PreInstalledSoftLabel);
+		this.add(combox);
 	}
 }
