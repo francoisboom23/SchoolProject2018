af0ac4006adb66e4d803892298834f48dbfb505f
francois <francois@francois-desktop>
2018-04-11 21:56:22 +0200

    gt

--- SchoolProject2018/src/TableInstalledSoft.java
+++ SchoolProject2018/src/TableInstalledSoft.java
@@ -15,17 +15,17 @@ public class TableInstalledSoft extends JPanel{
 	TableInstalledSoft(Connection connect,String sqlRequest){
 		setBounds(0,0,500,500);	
 		this.setBorder(BorderFactory.createLineBorder(Color.LIGHT_GRAY));
-//		try {
-//			PreparedStatement prepStat = connect.prepareStatement(sqlRequest);
-//			TableModelGen table = AccessBDGen.creerTableModel(prepStat);
-//		 	JTable table2 = new JTable(table);
-//		 	table2.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
-//		 	table2.setAutoResizeMode(JTable.AUTO_RESIZE_SUBSEQUENT_COLUMNS);
-//		 	JScrollPane scroll = new JScrollPane (table2) ;
-//		 	this.add(scroll);
-//			}
-//		catch(SQLException e) {	}
-		add(vincent =new JLabel("vincent"));
+		try {
+			PreparedStatement prepStat = connect.prepareStatement(sqlRequest);
+			TableModelGen table = AccessBDGen.creerTableModel(prepStat);
+		 	JTable table2 = new JTable(table);
+		 	table2.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
+		 	table2.setAutoResizeMode(JTable.AUTO_RESIZE_SUBSEQUENT_COLUMNS);
+		 	JScrollPane scroll = new JScrollPane (table2) ;
+		 	this.add(scroll);
+			}
+		catch(SQLException e) {	}
+//		add(vincent =new JLabel("vincent"));
 		setVisible(true);
 		}
 }
