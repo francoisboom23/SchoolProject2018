92ca81cf7870f1ca78e28ca8b2dd9d87e811c46c
francois <francois@francois-desktop>
2018-04-21 22:19:13 +0200

    fvb

--- SchoolProject2018/src/dateCombo.java
+++ SchoolProject2018/src/dateCombo.java
@@ -12,7 +12,8 @@ import javax.swing.*;
 
 public class dateCombo  extends JPanel{
 	private String[] dayList = {"1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30","31"};
-	private String[] monthList = {"January:","February","March","April:","May","June","July:","August","September","October:","November","December"};
+	//private String[] monthList = {"January:","February","March","April:","May","June","July:","August","September","October:","November","December"};
+	private String[] monthList = {"1","2","3","4","5","6","7:","8","9","10:","11","12"};
 	private String[] yearList = {"2000","2001","2002","2003","2004","2005","2006","2007","2008","2009","2010","2011","2012","2013","2014","2015","2016","2017","2018"};
 	private JComboBox day,month,year;
 	
@@ -28,16 +29,11 @@ public class dateCombo  extends JPanel{
 		add(day);
 		setVisible(true);
 	}
-	public Date getDate() {
-		String dateText = year.getSelectedItem()+"/"+month.getSelectedItem()+"/"+(String)day.getSelectedItem();
-		Date date=new Date(2000,10,10);
+	public Date getDate(){
+		String dateText = year.getSelectedItem()+"/"+month.getSelectedItem()+"/"+day.getSelectedItem();
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy/mm/dd");
-		try {
-			date = sdf.parse(dateText);
-		}
-		catch(Exception e){
-			System.out.println(e.getMessage());
-		}
+		java.util.Date date=sdf.parse(dateText);
+
 		return date;
 	}
 }
