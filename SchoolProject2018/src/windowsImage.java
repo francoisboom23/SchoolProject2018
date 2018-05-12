7f932ae3facc611d9ecb14b369cd162a62cdb236
francois <francois@francoislaptop>
2018-05-12 22:04:49 +0200

    fr

--- SchoolProject2018/src/windowsImage.java
+++ SchoolProject2018/src/windowsImage.java
@@ -10,7 +10,6 @@ import accessBD.*;
 public class windowsImage extends JPanel {
 	private JLabel image;
 	public windowsImage() {
-		setLayout(new FlowLayout());
 		image = new JLabel(new ImageIcon("/home/francois/Documents/Programmation/git/SchoolProject2018/a.jpg"));
 		add(image);
 		setVisible(true);
